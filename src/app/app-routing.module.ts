261de703ba7e8934f80bdfecf616ea240cba7c7e
HarishDass <harishdaas2002@gmail.com>
2023-08-31 12:23:30 +0530

    newChanges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'EmployeeDetails', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'Attendance', loadChildren: () => import('./attendance/attendance.module').then(m => m.AttendanceModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
