e5b4f72185681585c1a5a5cfbd78804822a1c5c3
Guru Prasadh <officialmailforguru@gmail.com>
2023-09-04 12:45:25 +0530

    Task Completed

--- src/app/attendance/attendance.module.ts
+++ src/app/attendance/attendance.module.ts
@@ -5,9 +5,10 @@ import { AttendanceRoutingModule } from './attendance-routing.module';
 import { AttendanceComponent } from './attendance.component';
 import { SharedModuleModule } from '../shared-module/shared-module.module';
 import { ViewTableComponent } from './view-table/view-table.component';
+import { SampleTableComponent } from './sample-table/sample-table.component';
 
 @NgModule({
-  declarations: [AttendanceComponent, ViewTableComponent],
+  declarations: [AttendanceComponent, ViewTableComponent, SampleTableComponent],
   imports: [
     CommonModule,
     AttendanceRoutingModule,
