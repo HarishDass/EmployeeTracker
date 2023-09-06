53539f6ef8673535b40e782b48d9cba2d9717a12
Kaushan05 <kaushan27052001@gmail.com>
2023-09-06 12:39:17 +0530

    Inactive Employe Data

--- src/app/employee-details/inactive-employee/inactive-employee.component.ts
+++ src/app/employee-details/inactive-employee/inactive-employee.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { EmployeData } from '../interface/employe-data';
 import { LeftoverTableService } from './Services/leftover-table.service';
-import { EmployeTableHeadData } from '../interface/employe-table-head-data';
 @Component({
   selector: 'app-inactive-employee',
   templateUrl: './inactive-employee.component.html',
@@ -9,33 +8,8 @@ import { EmployeTableHeadData } from '../interface/employe-table-head-data';
 })
 export class InactiveEmployeeComponent {
   leftoverdatas: EmployeData[] = [];
-  statuses: any[] = [
-    { label: 'Inactive', value: 'Inactive' },
-    { label: 'Active', value: 'Active' },
-  ];
-
-  inactive_emp_table_header: EmployeTableHeadData[] = [
-    {
-      headings: 'Photo',
-      field: 'Photo',
-    },
-    {
-      headings: 'Name',
-      field: 'employeeName',
-    },
-    {
-      headings: 'Employee Code',
-      field: 'EmployeeNo',
-    },
-    {
-      headings: 'Department',
-      field: 'deportment',
-    },
-    {
-      headings: 'Active Status',
-      field: 'Active',
-    },
-  ];
+  visible: boolean = false;
+  individualData: any = {};
 
   constructor(private LinkData: LeftoverTableService) {}
   ngOnInit() {
@@ -44,21 +18,10 @@ export class InactiveEmployeeComponent {
       console.log(this.leftoverdatas);
     });
   }
-  ansData: string = '';
-  getSeverity(status: boolean) {
-    if (status == false) {
-      this.ansData = 'Inactive';
-      return 'warning';
-    } else {
-      this.ansData = 'Active';
-      return 'success';
-    }
-  }
-  getSeverityFn(value: string) {
-    if (value == 'Inactive') {
-      return 'warning';
-    } else {
-      return 'success';
-    }
+
+  activateFn(lefted: EmployeData) {
+    this.visible = true;
+    this.individualData = lefted;
+    console.log(this.individualData);
   }
 }
--- src/app/employee-details/interface/employe-table-head-data.ts
+++
@@ -1,4 +0,0 @@
-export interface EmployeTableHeadData {
-  headings: string;
-  field: string;
-}
