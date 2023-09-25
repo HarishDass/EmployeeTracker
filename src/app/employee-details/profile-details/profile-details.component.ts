783fefb900991839d15a32c6674ba59fd59e7fc4
VINCENT08122001 <vincent08122001@gmail.com>
2023-09-25 16:13:40 +0530

    Employee details view page updation 2

--- src/app/employee-details/profile-details/profile-details.component.ts
+++ src/app/employee-details/profile-details/profile-details.component.ts
@@ -10,29 +10,23 @@ import { ProfileInterface } from '../profile-interface';
 export class ProfileDetailsComponent {
   first: number = 0;
   rows: number = 10;
-  onPageChange(event: any) {
-    this.first = event.first;
-    this.rows = event.rows;
-  }
-
   array: Array<ProfileInterface> = [];
   Inactive: any = {};
-  newdata: Array<ProfileInterface> = [];
-  visible: boolean = false;
-  ingredient!: boolean;
-  obj: any = {};
-  constructor(private datum: ProfileDetailServiceService) {}
 
+  constructor(private datum: ProfileDetailServiceService) {}
   ngOnInit(): void {
     this.datum.getData().subscribe((data: ProfileInterface[]) => {
-      console.log(data);
       this.array = data;
       console.log(this.array);
     });
   }
 
+  onPageChange(event: any) {
+    this.first = event.first;
+    this.rows = event.rows;
+  }
+
   showDialog(array: any) {
-    this.visible = true;
     this.Inactive = array;
     console.log(this.Inactive);
   }
