65ba1b5e0e8bc452a78ab3570161e6cf63ef7a43
Guru Prasadh <officialmailforguru@gmail.com>
2023-09-27 13:08:24 +0530

    update

--- src/app/attendance/attendance.component.ts
+++ src/app/attendance/attendance.component.ts
@@ -8,7 +8,6 @@ import { Component, Input } from '@angular/core';
 export class AttendanceComponent {
   value!: string;
   color: any;
-  @Input() page: any;
 
   pages = [
     { name: 'Attendance', path: 'home' },
